c2f300f627de06ed30525d3ba7823387c259c34a
Lucas Almeida <82480686+Holandapt@users.noreply.github.com>
2026-01-18 14:37:03 +0000

    Add files via upload

---
+++ src/exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiplica = (num1, num2) => num1 * num2;
+console.log(multiplica(2, 5));
+const dizOi = (nome) => `Ola, ${nome}`;
+console.log(dizOi("Eduardo"));
+//# sourceMappingURL=exercicio.js.map
---
+++ src/exercicio.ts
@@ -0,0 +1,6 @@
+const multiplica = (num1:number, num2:number) => num1 * num2
+console.log(multiplica(2,5));
+
+const dizOi = (nome:string) => 
+    `Ola, ${nome}`;
+console.log(dizOi("Eduardo"));
